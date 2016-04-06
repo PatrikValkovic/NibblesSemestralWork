c2e4a3633b727817f9f8ea2411a683a5c4deca76
Patrik Valkovic <pata.va@seznam.cz>
2016-04-06 13:52:26 +0200

    Add base file for ViewModels

---
+++ valkopat/src/ViewModels/BaseViewModel.h
@@ -0,0 +1,13 @@
+#ifndef CERVISEMESTRALKA_BASEVIEWMODEL_H
+#define CERVISEMESTRALKA_BASEVIEWMODEL_H
+
+namespace ViewModel
+{
+    class BaseViewModel
+    {
+    public:
+        virtual void ShowSplashScreen() = 0;
+    };
+}
+
+#endif //CERVISEMESTRALKA_BASEVIEWMODEL_H
