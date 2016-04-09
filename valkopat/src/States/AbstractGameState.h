7206926bcd4bf69ed156fd33cf1cb298b2537c48
Patrik Valkovic <pata.va@seznam.cz>
2016-04-09 13:40:22 +0200

    Added dependencies to ViewModels

--- valkopat/src/States/AbstractGameState.h
+++ valkopat/src/States/AbstractGameState.h
@@ -1,15 +1,26 @@
 #ifndef CERVISEMESTRALKA_ABSTRACTGAMESTATE_H
 #define CERVISEMESTRALKA_ABSTRACTGAMESTATE_H
+#include "../ViewModels/Abstract/BaseViewModel.h"
+
 
 namespace GameState
 {
     class AbstractGameState
     {
     public:
+        AbstractGameState(ViewModel::BaseViewModel* RenderingModel)
+                : RenderingModel(RenderingModel)
+        { }
+
         virtual ~AbstractGameState()
         { }
 
         virtual AbstractGameState* run() = 0;
+
+    protected:
+        ViewModel::BaseViewModel* RenderingModel;
     };
+
+
 }
 #endif //CERVISEMESTRALKA_ABSTRACTGAMESTATE_H
