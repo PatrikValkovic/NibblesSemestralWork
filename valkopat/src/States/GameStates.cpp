969fd4e859d1c6547c1bc6149fd1371d88861f43
Patrik Valkovic <pata.va@seznam.cz>
2016-04-11 20:44:28 +0200

    Right implemented of creating Game States

--- valkopat/src/States/GameStates.cpp
+++ valkopat/src/States/GameStates.cpp
@@ -3,12 +3,37 @@
 GameState::GameStates::GameStates(ViewModel::BaseViewModel* Model)
     :RenderingModel(Model)
 {
+    ChangeLanguageState* LanguageScreen = new ChangeLanguageState(this->RenderingModel);
+    EndState* EndScreen = new EndState(this->RenderingModel);
+    GamePauseState* PauseScreen = new GamePauseState(this->RenderingModel);
+    MenuGameState* MenuScreen = new MenuGameState(this->RenderingModel);
+    MultiplayerGameState* MultiplayerScreen = new MultiplayerGameState(this->RenderingModel);
+    NetGameState* NetScreen = new NetGameState(this->RenderingModel);
+    PlayingState* PlayScreen = new PlayingState(this->RenderingModel);
+    SingleplayerGameState* SingleScreen = new SingleplayerGameState(this->RenderingModel);
+    SplashScreenGameState* SplashScreen = new SplashScreenGameState(this->RenderingModel);
 
-    //MenuGameState* MenuScreen = new MenuGameState(this->RenderingModel);
-    //SplashScreenGameState* SplashScreen = new SplashScreenGameState(this->RenderingModel,MenuScreen);
+    LanguageScreen->AddStates(MenuScreen);
+    EndScreen->AddStates();
+    PauseScreen->AddStates(PlayScreen,MenuScreen);
+    MenuScreen->AddStates(MultiplayerScreen,SingleScreen,NetScreen,EndScreen,LanguageScreen);
+    MultiplayerScreen->AddStates(PlayScreen,MenuScreen);
+    NetScreen->AddStates(PlayScreen,MenuScreen);
+    PlayScreen->AddStates(MenuScreen,PauseScreen);
+    SingleScreen->AddStates(PlayScreen,MenuScreen);
+    SplashScreen->AddStates(MenuScreen);
 
+
+    this->AllStates.push_back(LanguageScreen);
+    this->AllStates.push_back(EndScreen);
+    this->AllStates.push_back(PauseScreen);
     this->AllStates.push_back(MenuScreen);
+    this->AllStates.push_back(MultiplayerScreen);
+    this->AllStates.push_back(NetScreen);
+    this->AllStates.push_back(PlayScreen);
+    this->AllStates.push_back(SingleScreen);
     this->AllStates.push_back(SplashScreen);
+
     this->First = SplashScreen;
 }
 
