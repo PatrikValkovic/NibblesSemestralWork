044d6ebb514fead5b4aad65120072d76e0d37ad5
Patrik Valkovic <pata.va@seznam.cz>
2016-04-11 18:23:33 +0200

    Added GamePauseState

--- valkopat/src/States/GamePauseState.cpp
+++ valkopat/src/States/GamePauseState.cpp
@@ -1,1 +1,15 @@
 #include "GamePauseState.h"
+
+GameState::GamePauseState::GamePauseState(ViewModel::BaseViewModel* RenderingModel,
+                                          GameState::PlayingState* StateOfGame)
+        : AbstractGameState(RenderingModel),
+          GameState(StateOfGame)
+{ }
+
+AbstractGameState* GameState::GamePauseState::run()
+{
+    return this->GameState;
+}
+
+
+
--- valkopat/src/States/GamePauseState.h
+++ valkopat/src/States/GamePauseState.h
@@ -1,11 +1,21 @@
 #ifndef CERVISEMESTRALKA_GAMEPAUSESTATE_H
 #define CERVISEMESTRALKA_GAMEPAUSESTATE_H
+#include "AbstractGameState.h"
+#include "PlayingState.h"
 
-
-class GamePauseState
+namespace GameState
 {
+    class GamePauseState : public AbstractGameState
+    {
+    public:
+        GamePauseState(ViewModel::BaseViewModel* RenderingModel, PlayingState* StateOfGame);
 
-};
+        virtual AbstractGameState* run();
+
+    private:
+        PlayingState* GameState;
+    };
+}
 
 
 #endif //CERVISEMESTRALKA_GAMEPAUSESTATE_H
