81691c00aa0ac3435b5f7a9f6b0b1851ca37748f
Patrik Valkovic <pata.va@seznam.cz>
2016-05-23 17:23:16 +0200

    Added PlayerSetting class into Setting class

--- valkopat/src/Game/Settings.cpp
+++ valkopat/src/Game/Settings.cpp
@@ -17,3 +17,8 @@ Game::Settings::~Settings()
     delete Instance;
     Instance = NULL;
 }
+
+void Game::Settings::GetAction(Game::Keys Key, int& Player, Game::Directions& Direction)
+{
+
+}
--- valkopat/src/Game/Settings.h
+++ valkopat/src/Game/Settings.h
@@ -1,6 +1,8 @@
 #ifndef CERVISEMESTRALKA_SETTINGS_H
 #define CERVISEMESTRALKA_SETTINGS_H
 #include <cstdlib>
+#include <vector>
+#include <map>
 #include "Enums.h"
 
 namespace Game
@@ -10,11 +12,19 @@ namespace Game
     private:
         Settings();
         static Settings* Instance;
+        struct PlayerSetting
+        {
+            int Playerindex;
+            std::map<Keys,Directions> Actions;
+        };
+        std::vector<PlayerSetting> IndividualSetting;
     public:
         Settings(const Settings&) = delete;
         Settings& operator=(const Settings& second) = delete;
         static Settings* GetInstance();
         ~Settings();
+
+        void GetAction(Keys Key,int& Player,Directions& Direction);
     };
 }
 
