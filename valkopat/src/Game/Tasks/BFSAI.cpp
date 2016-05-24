2f0f2b94af23e174fa37b238916042012d6b1959
Patrik Valkovic <pata.va@seznam.cz>
2016-05-24 18:11:28 +0200

    Added file to BFSAI class

---
+++ valkopat/src/Game/Tasks/BFSAI.cpp
@@ -0,0 +1,1 @@
+#include "BFSAI.h"
---
+++ valkopat/src/Game/Tasks/BFSAI.h
@@ -0,0 +1,17 @@
+#ifndef CERVISEMESTRALKA_BFSAI_H
+#define CERVISEMESTRALKA_BFSAI_H
+#include "BaseTask.h"
+
+namespace Game
+{
+    namespace Task
+    {
+        class BFSAI : public BaseTask
+        {
+
+        };
+    }
+}
+
+
+#endif //CERVISEMESTRALKA_BFSAI_H
