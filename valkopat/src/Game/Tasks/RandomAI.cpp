88fa7b9cc33f47fbd9998c49cdda8c87bba616a8
Patrik Valkovic <pata.va@seznam.cz>
2016-05-24 17:13:51 +0200

    Added RandomAI class with declarations

---
+++ valkopat/src/Game/Tasks/RandomAI.cpp
@@ -0,0 +1,14 @@
+#include "RandomAI.h"
+
+void Game::Task::RandomAI::run()
+{
+
+}
+
+Game::Task::RandomAI::RandomAI(Worm* AIWorm, GameContent* Content)
+{
+
+}
+
+
+
---
+++ valkopat/src/Game/Tasks/RandomAI.h
@@ -0,0 +1,25 @@
+#ifndef CERVISEMESTRALKA_RANDOMAI_H
+#define CERVISEMESTRALKA_RANDOMAI_H
+#include "BaseTask.h"
+#include "../GameContent.h"
+#include "../Worm.h"
+
+namespace Game
+{
+    namespace Task
+    {
+        class RandomAI
+        {
+        public:
+            RandomAI(Worm* AIWorm,GameContent* Content);
+            virtual void run();
+        private:
+            Worm* ControlledWorm;
+            GameContent* ContentOfGame;
+        };
+    }
+}
+
+
+
+#endif //CERVISEMESTRALKA_RANDOMAI_H
