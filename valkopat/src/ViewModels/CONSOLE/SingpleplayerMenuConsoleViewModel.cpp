8b355a4c791b9d8cbb665be2cb0b30b8f457a073
Patrik Valkovic <pata.va@seznam.cz>
2016-05-25 10:17:27 +0200

    Implemented SingleplayerMenuConsoleViewModel.evelofAI method

--- valkopat/src/ViewModels/CONSOLE/SingpleplayerMenuConsoleViewModel.cpp
+++ valkopat/src/ViewModels/CONSOLE/SingpleplayerMenuConsoleViewModel.cpp
@@ -1,18 +1,19 @@
 #include "SingpleplayerMenuConsoleViewModel.h"
 #ifdef USE_CONSOLE
 
-ViewModel::SingpleplayerMenuConsoleViewModel::SingpleplayerMenuConsoleViewModel(const Translate::TranslateEngine* Translate)
+ViewModel::SingpleplayerMenuConsoleViewModel::SingpleplayerMenuConsoleViewModel(
+        const Translate::TranslateEngine* Translate)
         : SingleplayerMenuAbstractViewModel(Translate)
 { }
 
 int ViewModel::SingpleplayerMenuConsoleViewModel::CountOfAI(int Max)
 {
     using namespace std;
-    while(true)
+    while (true)
     {
         cout << Translation->GetTranslation("CountOfAI") << ':';
         cin >> this->CountOfAIInGame;
-        if (cin.fail() || this->CountOfAIInGame > Max || this->CountOfAIInGame<0)
+        if (cin.fail() || this->CountOfAIInGame > Max || this->CountOfAIInGame < 0)
         {
             cout << Translation->GetTranslation("WrongEntry") << endl;
             cout << Translation->GetTranslation("MaxCount") << " " << Max << endl;
@@ -26,7 +27,7 @@ int ViewModel::SingpleplayerMenuConsoleViewModel::CountOfAI(int Max)
 std::string ViewModel::SingpleplayerMenuConsoleViewModel::NameOfPlayer()
 {
     using namespace std;
-    while(true)
+    while (true)
     {
         cout << Translation->GetTranslation("ChooseName") << ':';
         cin >> this->Name;
@@ -44,16 +45,16 @@ string ViewModel::SingpleplayerMenuConsoleViewModel::Level()
 {
     using namespace std;
     const vector<string> Levels = Game::PlaygroundFactory::GetAviableLevels();
-    while(true)
+    while (true)
     {
         cout << Translation->GetTranslation("AviableLevels") << endl;
         int index = 1;
-        for_each(Levels.begin(),Levels.end(),[&index,this](string i){
+        for_each(Levels.begin(), Levels.end(), [&index, this](string i) {
             try
             {
                 cout << index << ": " << Translation->GetTranslation(i) << endl;
             }
-            catch(Exceptions::InvalidArgumentException* e)
+            catch (Exceptions::InvalidArgumentException* e)
             {
                 delete e;
                 cout << index << ": " << i << endl;
@@ -62,19 +63,30 @@ string ViewModel::SingpleplayerMenuConsoleViewModel::Level()
         });
         cout << Translation->GetTranslation("LevelChoose") << ':';
         cin >> this->LevelIndex;
-        if(cin.fail() || this->LevelIndex<1 || this->LevelIndex>(int)Levels.size())
-        {
+        if (cin.fail() || this->LevelIndex<1 || this->LevelIndex>(int)Levels.size())
             cout << Translation->GetTranslation("WrongEntry") << endl;
-            continue;
-        }
-        break;
+        else
+            break;
     }
-    return Levels.at(this->LevelIndex-1);
+    return Levels.at((unsigned long)this->LevelIndex - 1);
 }
 
 string ViewModel::SingpleplayerMenuConsoleViewModel::LevelOfAI(std::vector<string> MenuEntries)
 {
-    return 0;
+    int AIIndex;
+    while (true)
+    {
+        for (int a = 0; a < (int) MenuEntries.size(); a++)
+            cout << a+1 << ': ' << Translation->GetTranslation(MenuEntries[a]) << endl;
+        cout << Translation->GetTranslation("ChooseAILevel") << endl;
+        cin >> AIIndex;
+        if (cin.fail() || AIIndex<1 || AIIndex>(int)MenuEntries.size())
+            cout << Translation->GetTranslation("WrongEntry") << endl;
+        else
+            break;
+    }
+    AIIndex--;
+    return MenuEntries.at((unsigned long)AIIndex);
 }
 
 
