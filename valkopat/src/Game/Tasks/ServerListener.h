c2b68358aa71649ef19abd6d89931e4832302a3c
Patrik Valkovic <pata.va@seznam.cz>
2016-05-29 19:19:29 +0200

    Added server listener class

---
+++ valkopat/src/Game/Tasks/ServerListener.h
@@ -0,0 +1,16 @@
+#ifndef CERVISEMESTRALKA_SERVERLISTENER_H
+#define CERVISEMESTRALKA_SERVERLISTENER_H
+
+namespace Game
+{
+    namespace Task
+    {
+        class ServerListener
+        {
+
+        };
+    }
+}
+
+
+#endif //CERVISEMESTRALKA_SERVERLISTENER_H
