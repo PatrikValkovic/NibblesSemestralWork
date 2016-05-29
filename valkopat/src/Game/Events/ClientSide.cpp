06ab04111eeb3e511f3ccc0887e4bb4375b50b75
Patrik Valkovic <pata.va@seznam.cz>
2016-05-29 11:31:01 +0200

    Added implementation of ClientSide.PlayerConnected

--- valkopat/src/Game/Events/ClientSide.cpp
+++ valkopat/src/Game/Events/ClientSide.cpp
@@ -103,9 +103,35 @@ Game::Worm* Game::Event::ClientSide::AskToWorm(string NameOfPlayer)
     return PlayerWorm;
 }
 
-Worm* Game::Event::ClientSide::PlayerConnected()
+Game::Worm* Game::Event::ClientSide::PlayerConnected()
 {
-    return NULL;
+    ServerActions Receive;
+    recv(SocketId,&Receive,sizeof(ServerActions),0);
+    if(Receive==ServerActions::StartGame)
+        return NULL;
+    if(Receive!=ServerActions::PlayerTransfer)
+        throw new Exceptions::ServerException("Invalid header of packet",__LINE__,__FILE__);
+
+    size_t LengthOfName;
+    recv(SocketId,&LengthOfName,sizeof(size_t),0);
+    char* Name = (char*)calloc(LengthOfName+1,1);
+    recv(SocketId,Name,LengthOfName,0);
+    string NameOfPlayer(Name);
+    free(Name);
+
+    int StartX;
+    int StartY;
+    Actions StartDirection;
+    int Index;
+    recv(SocketId,&StartX,sizeof(int),0);
+    recv(SocketId,&StartY,sizeof(int),0);
+    recv(SocketId,&StartDirection,sizeof(Actions),0);
+    recv(SocketId,&Index,sizeof(int),0);
+
+    Worm* Created = new Worm(StartX,StartY,StartDirection,Index);
+    Created->SetName(NameOfPlayer);
+
+    return Created;
 }
 
 
