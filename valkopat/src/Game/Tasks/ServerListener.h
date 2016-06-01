45222931407572e582ef55fcf88fa9404efebfe4
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 12:51:39 +0200

    Added comments to ServerListener

--- valkopat/src/Game/Tasks/ServerListener.h
+++ valkopat/src/Game/Tasks/ServerListener.h
@@ -13,14 +13,37 @@ namespace Game
 {
     namespace Task
     {
+        /**
+         * Listen server and move Worms by server requests
+         */
         class ServerListener : public BaseTask
         {
         public:
+            /**
+             * Create new instance of ServerListener
+             * @param ClientSocket Socket of client
+             * @param Worms Worms to control
+             * @param PlayerId ID of player playing on current PC
+             * @return New instance of ServerListener
+             */
             ServerListener (int ClientSocket,vector<Worm*> Worms,int PlayerId);
+
+            /**
+             * Move worms until Game::Server::Wait occurs
+             */
             virtual void run();
         private:
+            /**
+             * Client socket
+             */
             int CliSocket;
+            /**
+             * Id of player playing on current PC
+             */
             int IdOfCurrentPlayer;
+            /**
+             * Controlled worms
+             */
             vector<Worm*> InGameWorms;
         };
     }
