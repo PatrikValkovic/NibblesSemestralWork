0a8247baa9071347d9a499f10887260dacfc10c3
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:36:08 +0200

    Added comment to PlayingState

--- valkopat/src/States/PlayingState.h
+++ valkopat/src/States/PlayingState.h
@@ -10,33 +10,89 @@
 
 namespace GameStates
 {
+    /**
+     * Represent state with game loop
+     */
     class PlayingState : public AbstractGameState
     {
     public:
+        /**
+         * Create new instance of PlayingState
+         * @param RenderingModel Rendering submodule
+         * @return New instance of PlayingState
+         */
         PlayingState(ViewModel::BaseViewModel* RenderingModel);
 
+        /**
+         * Destructor delete Game::GameContent
+         */
         ~PlayingState();
 
+        /**
+         * Run game loop
+         * @return Next state to run
+         */
         virtual AbstractGameState* run();
 
+        /**
+         * Fill needed states
+         * @param MenuState State with main game menu
+         * @param PauseState Pause state
+         */
         void AddStates(MenuGameState* MenuState, GamePauseState* PauseState);
 
+        /**
+         * Delete old Game::GameContent and replace it with new one
+         * @param NewContent New content of game
+         */
         void ClearContent(Game::GameContent* NewContent);
 
-        void ValidatePositionsOfWorms();
-
+        /**
+         * Check if still existing playable worm
+         * @return True if still exists playable worm, false otherwise
+         */
         bool Playing();
 
+        /**
+         * Return all worms in current Game::GameContent (shallow copy)
+         * @return Vector of all Game::Worm
+         */
         std::vector<Game::Worm*> GetWorms();
 
     private:
         MenuGameState* Menu;
         GamePauseState* Pause;
+
+        /**
+         * Current content of game
+         */
         Game::GameContent* ContentOfGame = NULL;
+
+        /**
+         * Move worms in their directions
+         */
         void MoveWorms();
+
+        /**
+         * Check if there are events to run and proccess them
+         * @return
+         */
         bool ProccessEvents();
+
+        /**
+         * Check if there are overlaps objects and resolve it
+         */
         void CheckCollisions();
+
+        /**
+         * Run all tasks in current Game::GameContent
+         */
         void RunTasks();
+
+        /**
+         * Check if worm is out of playiled and if yes, move it the other side of playiled
+         */
+        void ValidatePositionsOfWorms();
     };
 }
 
