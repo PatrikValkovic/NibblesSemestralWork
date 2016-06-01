02d41e34a582ce6c8ab2a662314f785c150e6944
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:56:53 +0200

    Added comments to AbstractEvent

--- valkopat/src/Game/Events/AbstractEvent.h
+++ valkopat/src/Game/Events/AbstractEvent.h
@@ -6,13 +6,28 @@ namespace Game
 {
     namespace Event
     {
+        /**
+         * Base abstract class for Events
+         */
         class Abstract
         {
         public:
+
+            /**
+             * Destructor
+             */
             virtual ~Abstract(){}
 
+            /**
+             * Check if are events to proccess
+             * @return True if are events to proccess, false otherwise
+             */
             virtual bool HasActions() = 0;
 
+            /**
+             * Proccess all waiting events
+             * @return False if game should end, true if continue
+             */
             virtual bool ProccessActions() = 0;
         };
     }
