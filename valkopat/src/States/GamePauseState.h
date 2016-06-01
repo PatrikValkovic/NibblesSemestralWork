5e944be108d767da6c5aa234e9dae9322dda42b9
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:01:53 +0200

    Added comments to GamePauseState

--- valkopat/src/States/GamePauseState.h
+++ valkopat/src/States/GamePauseState.h
@@ -7,13 +7,30 @@
 
 namespace GameStates
 {
+    /**
+     * State to show pause and final score
+     */
     class GamePauseState : public AbstractGameState
     {
     public:
+        /**
+         * Create new instance of GamePauseState
+         * @param RenderingModel Rendering submodule
+         * @return New instance of GamePauseState
+         */
         GamePauseState(ViewModel::BaseViewModel* RenderingModel);
 
+        /**
+         * Show score of players
+         * @return Next state to run
+         */
         virtual AbstractGameState* run();
 
+        /**
+         * Filling needed states
+         * @param StateOfGame State of game
+         * @param MenuState State of menu
+         */
         void AddStates(PlayingState* StateOfGame, MenuGameState* MenuState);
 
     private:
