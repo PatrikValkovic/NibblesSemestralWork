51d50aa2fe46d12ce8962fbda3766462044f7b40
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 11:39:27 +0200

    Added comments to BaseAITask

--- valkopat/src/Game/Tasks/BaseAITask.h
+++ valkopat/src/Game/Tasks/BaseAITask.h
@@ -8,13 +8,36 @@ namespace Game
 {
     namespace Task
     {
+        /**
+         * Represent base abstract class to inherit by AI
+         */
         class BaseAITask : public BaseTask
         {
         protected:
+            /**
+             * Worm to controll
+             */
             Worm* ControlledWorm;
+
+            /**
+             * Current content of game
+             */
             GameContent* ContentOfGame;
         public:
+            /**
+             * Create new instance of BaseAITask
+             * @param WormToControl Game::Worm to control
+             * @param Game Game::GameContent Current content of game
+             * @return New instance of BaseAITask
+             */
             BaseAITask(Worm* WormToControl,GameContent* Game);
+
+            /**
+             * Creates new instance of same class
+             * @param WormToControl WormToControl
+             * @param Game Current content of game
+             * @return New instance of same type as inherited type
+             */
             virtual BaseAITask* CreateInstance(Worm* WormToControl,GameContent* Game) = 0;
         };
     }
