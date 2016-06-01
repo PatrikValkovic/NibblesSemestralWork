f85991b32236551e3ec7b598639cd7ab4d8c46d8
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 11:03:00 +0200

    Added comments to EventContainer

--- valkopat/src/Game/Events/EventContainer.h
+++ valkopat/src/Game/Events/EventContainer.h
@@ -8,13 +8,38 @@ namespace Game
 {
     namespace Event
     {
+        /**
+         * Container for events
+         */
         class EventContainer
         {
+            /**
+             * Vector of all events
+             */
             std::vector<Abstract*> Events;
         public:
+            /**
+             * Run all events
+             * @return
+             */
             bool ProccessEvents();
+
+            /**
+             * Add event to Container
+             * @param E Event to add
+             */
             void AddEvent(Abstract* E);
+
+            /**
+             * Remove event from container
+             * @param E Event to remove
+             * @return True if was event removed, false otherwise
+             */
             bool RemoveEvent(Abstract* E);
+
+            /**
+             * Remove and delete all events in container
+             */
             void DeleteAllEvents();
         };
     }
