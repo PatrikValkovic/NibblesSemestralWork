61bd4e6adb4abb4d89e9ecd53e5ecd0203acb562
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 11:44:30 +0200

    Added comments to BFSAI

--- valkopat/src/Game/Tasks/BFSAI.h
+++ valkopat/src/Game/Tasks/BFSAI.h
@@ -11,15 +11,47 @@ namespace Game
 {
     namespace Task
     {
+        /**
+         * AITask that inernally use BFS
+         */
         class BFSAI : public BaseAITask
         {
         public:
+            /**
+             * Create new instance of BFSAI
+             * @param AIWorm Game::Worm to control
+             * @param Content Game::GameContent of game
+             * @return New instance of BFSAI
+             */
             BFSAI(Worm* AIWorm,GameContent* Content);
+
+            /**
+             * Run BFS and find the shorted path
+             */
             virtual void run();
+
             virtual BaseAITask* CreateInstance(Worm* WormToControl,GameContent* Game);
         private:
+            /**
+             * Run BFS
+             * @param map Representation of gamefield
+             * @param BeginOfSearch Where begin search
+             * @param EndPoint Where end search
+             * @return Direction to move
+             */
             Actions BFS(uint32_t** map, Point BeginOfSearch, Point EndPoint);
+            /**
+             * Generate points around specific Game::Point, works also in the edge of gamefield
+             * @param AroundToGenerate Game::Point around which generate points
+             * @return Vector of poitns around
+             */
             vector<Point> GeneratePointsAround(Point AroundToGenerate);
+            /**
+             * Choose direction, which should worm move
+             * @param From From which point
+             * @param To To which point
+             * @return Direction to move
+             */
             Actions ResolveDirection(Point From, Point To);
         };
     }
