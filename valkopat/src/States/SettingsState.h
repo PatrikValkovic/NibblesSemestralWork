5f511fb03e0238074da1d0433a28043be6d69ed6
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:48:29 +0200

    Added comments to SettingState

--- valkopat/src/States/SettingsState.h
+++ valkopat/src/States/SettingsState.h
@@ -6,13 +6,29 @@
 
 namespace GameStates
 {
+    /**
+     * State where player could change his setting
+     */
     class SettingsState : public AbstractGameState
     {
     public:
+        /**
+         * Create new instance of SettingState
+         * @param RenderingModel Rendering submodule
+         * @return New instance of SettingState
+         */
         SettingsState(ViewModel::BaseViewModel* RenderingModel);
 
+        /**
+         * Change language or keybinding
+         * @return State with game menu
+         */
         virtual AbstractGameState* run();
 
+        /**
+         * Fill needed states
+         * @param MenuState State with game menu
+         */
         void AddStates(MenuGameState* MenuState);
 
     private:
