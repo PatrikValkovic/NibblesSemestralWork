24b2a21a0274d067af02e9822bb9203827548300
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:13:07 +0200

    Added comments to MultiplayerGameState

--- valkopat/src/States/MultiplayerGameState.h
+++ valkopat/src/States/MultiplayerGameState.h
@@ -4,15 +4,31 @@
 
 namespace GameStates
 {
-    class MenuGameState;
-
+    /**
+     * State what prepare game for playing more player on one PC
+     */
     class MultiplayerGameState : public AbstractGameState
     {
     public:
+
+        /**
+         * Create new instance of MultiplayerGameState
+         * @param RenderingModel Rendering submodule
+         * @return New instance of MultiplayerGameState
+         */
         MultiplayerGameState(ViewModel::BaseViewModel* RenderingModel);
 
+        /**
+         * Get informations from users and prepare game to run
+         * @return Next state to run
+         */
         virtual AbstractGameState* run();
 
+        /**
+         * Fill needed states
+         * @param PlayState State with main loop of game
+         * @param MenuState Main game menu state
+         */
         void AddStates(PlayingState* PlayState, MenuGameState* MenuState);
 
     private:
