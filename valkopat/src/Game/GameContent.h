3fffc1e229351904b9d32867bf79f7f78b9642ff
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 13:03:16 +0200

    Added comments to GameContent

--- valkopat/src/Game/GameContent.h
+++ valkopat/src/Game/GameContent.h
@@ -16,25 +16,76 @@ using Game::Event::EventContainer;
 
 namespace Game
 {
+    /**
+     * Container for all objects in game
+     */
     class GameContent
     {
     private:
+        /**
+         * Where is actual position of food
+         */
         Point Food;
     public:
+        /**
+         * Create new empty instance of GameContent
+         * @return New instance of GameContent
+         */
         GameContent();
+
+        /**
+         * Create new instance of GameContent with Worms and Playground
+         * @param Worms Worms controlled by PC
+         * @param Player Player Game::Worm
+         * @param Playground Playground to use
+         * @return New instance of GameContent
+         */
         GameContent(vector<Worm*> Worms,Worm* Player, PlayGround* Playground);
+
+        /**
+         * Desctructor delete all objects in game include events, tasks, worms and playground
+         */
         ~GameContent();
 
+        /**
+         * Current playing playground
+         */
         PlayGround* Ground;
+
+        /**
+         * All worms in game, include player worms
+         */
         vector<Worm*> Worms;
-        map<Worm*,Actions> MovingDirections;
+
+        /**
+         * Event container containing all events in game
+         */
         EventContainer Events;
+
+        /**
+         * Vector of Game::Task::BaseTask containing all tasks in game
+         */
         vector<Game::Task::BaseTask*> Tasks;
 
+        /**
+         * Get current position of food
+         * @return Game::Point Position of food
+         */
         Point GetFood() const;
+        /**
+         * Generate new position of Food
+         */
         void GenerateFood();
 
+        /**
+         * Create array of size char[HeightOfGame][WidthOfGame] allocated by new char[]
+         * @return Array for playfield
+         */
         char** CreateArrayForGame();
+        /**
+         * Method to delete array previously created by Game::GameContent::CreateArrayForGame
+         * @param Array Array to delete
+         */
         void DeleteArrayForGame(char**& Array);
     };
 }
