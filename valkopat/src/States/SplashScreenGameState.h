3ca9827a249e8964fcbddc7fead3bfa42c5a36fe
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 10:52:30 +0200

    Added comments to SplashScreenGameState

--- valkopat/src/States/SplashScreenGameState.h
+++ valkopat/src/States/SplashScreenGameState.h
@@ -7,16 +7,36 @@
 
 namespace GameStates
 {
+    /**
+     * Starting state, show welcome message
+     */
     class SplashScreenGameState : public AbstractGameState
     {
     public:
+        /**
+         * Create new instance of SplashScreenGameState
+         * @param RenderingModel Rendering submodule
+         * @return New instance of SplashScreenGameState
+         */
         SplashScreenGameState(ViewModel::BaseViewModel* RenderingModel);
+
+        /**
+         * Show welcome message, wait for a specific time and return next state
+         * @return State with game menu
+         */
         virtual AbstractGameState* run();
 
+        /**
+         * Fill needed states
+         * @param NextState State with game menu
+         */
         void AddStates(MenuGameState* NextState);
 
     private:
         MenuGameState* NextState;
+        /**
+         * How long wait to show menu
+         */
         const uint32_t SleepForSecond;
     };
 }
