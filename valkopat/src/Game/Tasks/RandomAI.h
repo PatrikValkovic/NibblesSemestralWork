7e3c6f8be41a1d62570ccb3f835186b1afcea3fd
Patrik Valkovic <pata.va@seznam.cz>
2016-06-01 12:48:49 +0200

    Added comments to RandomAI task

--- valkopat/src/Game/Tasks/RandomAI.h
+++ valkopat/src/Game/Tasks/RandomAI.h
@@ -7,10 +7,23 @@ namespace Game
 {
     namespace Task
     {
+        /**
+         * Task for AI with random movement
+         */
         class RandomAI : public BaseAITask
         {
         public:
+            /**
+             * Create new instance of RandomAI
+             * @param AIWorm Controlled Game::Worm
+             * @param Content Game::GameContent Content of game
+             * @return New instance of RandomAI
+             */
             RandomAI(Worm* AIWorm,GameContent* Content);
+
+            /**
+             * Change Game::Worm directory randomly
+             */
             virtual void run();
             virtual BaseAITask* CreateInstance(Worm* WormToControl,GameContent* Game);
         };
